a628eda8441bd428c9b185965bf516cf65398d7f
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2020-02-14 12:28:47 +0200

    feat: add test project and samples

---
+++ BlazorTablePerformance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorTablePerformance
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTelerikBlazor();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blazor-client-performance
 This repository aims to investigate how certain features of the component model in blazor affect the performance in wasm scenarios
+
+To test the performance of each Grid/Table, click on a row and observe the latency before it's marked as selected by the background-color style
