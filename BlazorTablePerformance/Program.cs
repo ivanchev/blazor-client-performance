8739e70667d4856fa0cd99a5ae8e136271c6a7c8
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2020-05-22 12:06:51 +0300

    chore: update to webassembly 3.2.0

--- BlazorTablePerformance/Program.cs
+++ BlazorTablePerformance/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
-using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorTablePerformance
 {
@@ -13,6 +16,8 @@ namespace BlazorTablePerformance
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddTelerikBlazor();
 
